058632c8b4317188e33f95ca32c79694ac241438
Facundo Palombo facundo.palombo@gmail.com <facundo.palombo@gmail.com>
2024-03-11 13:36:20 +0100

    io

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Por favor introduce tu nombre");
+    let mut nombre: String = String::new();
+    std::io::stdin().read_line(&mut nombre).unwrap();
+    nombre = nombre.trim().to_string();
+
+    println!("Por favor introduce tu edad");
+    let mut edad: String = String::new();
+    std::io::stdin().read_line(&mut edad).unwrap();
+    let edad_int: u8 = edad.trim().parse().unwrap();
+
+    println!("Hola bienvenido {nombre} de {edad_int} años a Rust")
 }
