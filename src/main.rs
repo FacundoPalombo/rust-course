13047e04aa43c249d54ea723664196c9736d287e
Facundo Palombo facundo.palombo@gmail.com <facundo.palombo@gmail.com>
2024-03-11 13:59:56 +0100

    control structures

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 fn main() {
-    println!("Por favor introduce tu nombre");
-    let mut nombre: String = String::new();
-    std::io::stdin().read_line(&mut nombre).unwrap();
-    nombre = nombre.trim().to_string();
-
-    println!("Por favor introduce tu edad");
+    println!("Por  favor introduce tu edad");
     let mut edad: String = String::new();
     std::io::stdin().read_line(&mut edad).unwrap();
+
+    // Convertir esa edad a un numero
     let edad_int: u8 = edad.trim().parse().unwrap();
 
-    println!("Hola bienvenido {nombre} de {edad_int} años a Rust")
+    if edad_int >= 18 {
+        println!("Bien, ¿llevas puestas zapatillas de lona? Y/n");
+        let mut zapatillas_de_lona: String = String::new();
+        std::io::stdin().read_line(&mut zapatillas_de_lona).unwrap();
+        
+        let opt: String = zapatillas_de_lona.trim().to_uppercase().to_string();
+
+
+        if opt == "Y" {
+            println!("Al boliche con zapatillas de lona no pasas pibe...");
+        } else {
+            println!("Pasa papá");
+        }
+    } else {
+        println!("Al boliche no pasas nene");
+    }
 }
